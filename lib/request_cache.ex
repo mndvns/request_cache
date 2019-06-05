9e303f56cd827bc1972e99a910ebd2994eafbff9
Michael Vanasse <michaelvanasse@gmail.com>
2019-06-05 11:46:03 -0600

    simplify

--- lib/request_cache.ex
+++ lib/request_cache.ex
@@ -5,34 +5,6 @@ defmodule RequestCache do
     delete(state_uuid())
   end
 
-  def count() do
-    length(list(:structs))
-  end
-
-  def list(group \\ :structs)
-
-  def list(:all) do
-    req_id = state_uuid()
-
-    ets()
-    |> :ets.tab2list()
-    |> Enum.find_value([], fn {k, v} -> if k == req_id, do: v end)
-  end
-
-  def list(:structs) do
-    list(:all)
-    |> Stream.reject(fn {k, _} -> is_binary(k) end)
-    |> Stream.map(fn {_, v} -> v end)
-    |> Enum.into([])
-  end
-
-  def list(:references) do
-    list(:all)
-    |> Stream.filter(fn {k, _} -> is_binary(k) end)
-    |> Stream.map(fn {_, v} -> v end)
-    |> Enum.into([])
-  end
-
   def find(%{__struct__: mod} = var) do
     find(mod, Map.from_struct(var))
   end
@@ -55,11 +27,12 @@ defmodule RequestCache do
     key = state_encode_key(key_parts)
 
     case state_has_key?(key) do
-      false -> handle.({mod, args, ident, key})
       true -> state_get(key)
+      false ->
+        value = handle.({mod, args, ident, key})
+        state_put(key, value)
+        value
     end
-    |> (&state_get_struct(mod, &1)).()
-    |> (&state_put(key, mod, &1)).()
   end
 
   def remove(%{__struct__: mod, id: id}) do
@@ -79,49 +52,10 @@ defmodule RequestCache do
     Map.get(state_all(), key)
   end
 
-  defp state_get_struct(mod, %{__struct__: mod} = val) do
-    val
-  end
-
-  defp state_get_struct(mod, %{} = conf) do
-    struct(mod, conf)
-  end
-
-  defp state_get_struct(mod, {mod, id}) do
-    state_get_struct(mod, id)
-  end
-
-  defp state_get_struct(mod, list) when is_list(list) do
-    Enum.map(list, &state_get_struct(mod, &1))
-  end
-
-  defp state_get_struct(mod, id) do
-    case state_get({mod, id}) do
-      %{__struct__: _} = val -> val
-      %{} = conf -> struct(mod, conf)
-      other -> other
-    end
-  end
-
   defp state_put(key, val) do
     state = state_all()
     updated = Map.put(state, key, val)
     put(state_uuid(), updated)
-  end
-
-  defp state_put(key, mod, list) when is_list(list) do
-    Enum.map(list, &state_put(key, mod, &1))
-  end
-
-  defp state_put(key, mod, %{__struct__: mod, id: id} = val) do
-    state_put(key, {mod, id})
-    state_put({mod, id}, val)
-    val
-  end
-
-  defp state_put(_key, mod, {mod, id} = val) do
-    state_get({mod, id})
-    val
   end
 
   defp state_delete(key) do
