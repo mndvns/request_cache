89c7777224ad65e40123a13046ec60cde8c64914
Michael Vanasse <michaelvanasse@gmail.com>
2019-06-07 06:24:15 -0600

    remove wrapper and improve key reliability

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # RequestCache
 
-A cache local to Plug requests.
+A cache that stores items per Plug request. Built on top of [ConCache](https://github.com/sasa1977/con_cache).
+
+## Usage
+
+For best results, add the RequestCache plug to your plug pipeline:
+
+```
+plug(RequestCache.Plug)
+
+```
+
+You can use it without plug. It will key off of the main
+process pid instead off of a request.
 
 ## Installation
 
--- lib/request_cache.ex
+++ lib/request_cache.ex
@@ -1,78 +1,84 @@
 defmodule RequestCache do
-  use RequestCache.Wrapper
+  # get all ConCache functions
+  omit = ~w(__struct__ start_link start)a
+  funs = ConCache.__info__(:functions) |> Keyword.drop(omit)
+
+  # rewrite functions here, setting `__MODULE__` as first argument for all
+  for {fun, arity} <- funs do
+    args = 1..arity |> Enum.map(&Macro.var(:"arg_#{&1 - 1}", nil)) |> tl()
+    def unquote(fun)(unquote_splicing(args)) do
+      ConCache.unquote(fun)(__MODULE__, unquote_splicing(args))
+    end
+    defoverridable [{fun, arity - 1}]
+  end
+
+  def start_link(gen_server_options \\ []) do
+    [
+      global_ttl: Application.get_env(:request_cache, :global_ttl, 1_000),
+      ttl_check_interval: Application.get_env(:request_cache, :global_ttl, 250),
+      ets_options: Application.get_env(:request_cache, :ets_options, [read_concurrency: true])
+    ]
+    |> Keyword.put(:name, __MODULE__)
+    |> Keyword.merge(gen_server_options)
+    |> ConCache.start_link()
+  end
+
+  def get(key) when is_tuple(key) do
+    super({request_uuid(), key})
+  end
+
+  def get_or_store(key, fun) do
+    super({request_uuid(), key}, fun)
+  end
+
+  def put(key, value) do
+    super({request_uuid(), key}, value)
+  end
+
+  def update(key, fun) do
+    super({request_uuid(), key}, fun)
+  end
+
+  def delete(key) do
+    super({request_uuid(), key})
+  end
+
+  def touch(key) do
+    super({request_uuid(), key})
+  end
+
+  def all() do
+    uuid = request_uuid()
+    ets()
+    |> :ets.tab2list()
+    |> Enum.reduce(%{}, fn
+      {{^uuid, key}, value}, acc -> Map.put(acc, key, value)
+      _, acc -> acc
+    end)
+  end
+
+  def list() do
+    all() |> Enum.into([])
+  end
+
+  def size() do
+    all() |> map_size()
+  end
+
+  def keys() do
+    all() |> Map.keys()
+  end
+
+  def values() do
+    all() |> Map.values()
+  end
 
   def clear() do
-    delete(state_uuid())
+    keys() |> Enum.map(&delete/1)
+    :ok
   end
 
-  def find(%{__struct__: mod} = var) do
-    find(mod, Map.from_struct(var))
-  end
-
-  def find(mod) do
-    find(mod, %{})
-  end
-
-  def find(mod, args) do
-    find(mod, args, nil)
-  end
-
-  def find(mod, args, ident) do
-    handle = fn {mod, args, _ident, _key} -> struct(mod, args) end
-    find(mod, args, ident, handle)
-  end
-
-  def find(mod, args, ident, handle) do
-    key_parts = {mod, args, ident}
-    key = state_encode_key(key_parts)
-
-    case state_has_key?(key) do
-      true -> state_get(key)
-      false ->
-        value = handle.({mod, args, ident, key})
-        state_put(key, value)
-        value
-    end
-  end
-
-  def remove(%{__struct__: mod, id: id}) do
-    case state_has_key?({mod, id}) do
-      true -> state_delete({mod, id})
-      _ -> nil
-    end
-  end
-
-  defp state_all() do
-    req_id = state_uuid()
-    state = get(req_id)
-    state || %{}
-  end
-
-  defp state_get(key) do
-    Map.get(state_all(), key)
-  end
-
-  defp state_put(key, val) do
-    state = state_all()
-    updated = Map.put(state, key, val)
-    put(state_uuid(), updated)
-  end
-
-  defp state_delete(key) do
-    state = state_all()
-    updated = Map.delete(state, key)
-    put(state_uuid(), updated)
-  end
-
-  defp state_has_key?(key) do
-    Map.has_key?(state_all(), key)
-  end
-
-  defp state_encode_key(term) do
-    :crypto.hash(:sha256, :erlang.term_to_binary(term)) |> Base.encode64()
-  end
-
-  def state_uuid() do
-    Process.get(:request_cache_id, UUID.uuid3(:oid, inspect(self)))
+  defp request_uuid() do
+    Process.get(:request_cache_id, UUID.uuid3(:oid, inspect(self())))
   end
 end
